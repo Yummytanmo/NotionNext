59a0dc667e48eb8af0ca7f62cbf4eed6b8f28a4d
Yummytanmo <147129696+Yummytanmo@users.noreply.github.com>
2025-06-02 03:30:27 +0800

    fix: update site creation date and hero section content in theme configuration

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,7 +4,7 @@ const CONFIG = {
 
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2025-06-01', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
@@ -18,12 +18,12 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
+  HEO_HERO_TITLE_1: '探索未来',
+  HEO_HERO_TITLE_2: '探索AI的无限可能',
+  HEO_HERO_TITLE_3: 'YUMMYTANMO.COM',
   HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_5: '爱乐智呈 虚拟乐器演奏平台',
+  HEO_HERO_TITLE_LINK: 'https://hitaiyuezhicheng.com',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
@@ -41,19 +41,19 @@ const CONFIG = {
   HEO_INFOCARD_GREETINGS: [
     '你好！我是',
     '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+    '💡 探索AI的无限可能',
+    '🌟 让我们一起学习',
+    '🚀 让我们一起成长',
+    '🌈 让我们一起探索',
+    '🎉 欢迎来到我的博客',
   ],
 
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://github.com/Yummytanmo',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_URL3: 'https://github.com/yummytanmo.github.io',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
@@ -117,10 +117,10 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_1: '微信公众号',
+  HEO_SOCIAL_CARD_TITLE_2: '关注微信公众号了解更多',
+  HEO_SOCIAL_CARD_TITLE_3: '点击关注公众号',
+  HEO_SOCIAL_CARD_URL: 'https://yummytanmo.fun/weixin', // 微信公众号二维码链接
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
